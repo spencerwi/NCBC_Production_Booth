e280874b51025027b19797140b46fb2ee204cca8
spencerwi <spencerwi@gmail.com>
2022-07-02 11:25:31 -0400

    Trying to fix automated PDF generation

--- run-pandoc.sh
+++ run-pandoc.sh
@@ -2,4 +2,6 @@
 
 # Entry point script used by the Github action that automatically generates our PDFs for printing any time there's a change.
 
-make all
+cd Playbook && pandoc --toc 1_sunday-morning-setup.md troubleshooting.md -o ../Playbook.pdf --filter mermaid-filter --variable=fontfamily:arev -H ../chapter_break.tex
+cd ..
+cd Manual && pandoc --toc connections-mac.md connections-pc.md -o ../Manual.pdf --filter mermaid-filter --variable=fontfamily:arev -H ../chapter_break.tex
